337e6b052d6c4d7da3863c766813295775cefe86
Lucas <lucas.martins@spassu.com.br>
2022-07-05 20:28:33 -0300

    [ADD] Error Treatment

--- app.js
+++ app.js
@@ -1,10 +1,29 @@
 const express = require('express')
 const app = express()
+const morgan = require('morgan')
 
 const productRouter = require('./routes/produtos')
 const orderRouter = require('./routes/pedidos')
 
+app.use(morgan('dev'))
+
 app.use('/produtos', productRouter)
 app.use('/pedidos', orderRouter)
+
+// Quando não encontra rota, entra aqui
+app.use((req, res, next) => {
+    const error = new Error('Não encontrado')
+    error.status = 404
+    next(error)
+})
+
+app.use((error, req, res, next) => {
+    res.status(error.status || 500)
+    return res.send({
+        error: {
+            message: error.message
+        }
+    })
+})
 
 module.exports = app
